9fd2bb99753ea90f96e33ae476d148d507eed8a6
Aleksey Redkous <aredkous@ozon.ru>
2022-04-22 16:53:53 +0300

    Second lesson

--- auto_back.py
+++ auto_back.py
@@ -18,3 +18,22 @@ class Auto_back(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, 180)
         self.rect = self.image.get_rect()
         self.rect.center = (random.choice(self.points), 0)
+        self.max_speed = 25
+        self.min_speed = 2
+        self.speed = random.randint(self.min_speed, self.max_speed)
+        self.global_speed = 0
+        self.global_min_speed = 0
+        self.global_max_speed = 50
+        self.rect.y = random.randrange(-height, 0, 300)
+
+    def update(self):
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_UP] and self.global_speed < self.global_max_speed:
+            self.global_speed += 1
+        if keystate[pygame.K_DOWN] and self.global_speed > self.global_min_speed:
+            self.global_speed -=1
+        self.rect.y += self.speed + self.global_speed
+        if self.rect.top > height * 2:
+            self.speed = random.randint(self.min_speed, self.max_speed)
+            self.rect.centerx = random.choice(self.points)
+            self.rect.y = random.randrange(-height, 0, 300)
